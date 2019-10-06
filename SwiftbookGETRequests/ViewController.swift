057bfa2f051ea9754b61ec52946346afeff474b6
Vladimir Avstirychenko <liquid-05@mail.ru>
2019-10-06 20:23:56 +0300

    Initial Commit

---
+++ SwiftbookGETRequests/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftbookGETRequests
+//
+//  Created by Владимир on 06/10/2019.
+//  Copyright © 2019 VladCorp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
